1f191ee2c192e79bc6e12b065c755a179f0c6b79
fullstack <apachish@gmail.com>
2024-11-16 00:10:38 +0330

    up

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>مشاوره زندگی</title>
+
+
+    </head>
+    <body class="rtl page-template page-template-elementor_header_footer page page-id-38217 wp-custom-logo wp-embed-responsive theme-consultino woocommerce-no-js woo-variation-swatches wvs-behavior-blur wvs-theme-consultino wvs-show-label wvs-tooltip consultino consultino-consultino elementor-default elementor-template-full-width elementor-kit-5 elementor-page elementor-page-38217">
+    <!-- overlay -->
+
+        <!-- #page -->
+    <img src="/images/logo.png">
+    </body>
+    
+</html>
